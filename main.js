e2ff5982fd6788442c89d4e28c11a3cf2164bed0
iripat <iripat@waresix.com>
2021-12-12 11:00:31 +0800

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.135.0-pjGUcRG9Xt70OdXl97VF/mode=imports,min/optimized/three.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
